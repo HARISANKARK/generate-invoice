4727d45104c59cdd9e0e9237ebfb802f77a3c2b7
HARISANKARK <hari404832@gmail.com>
2024-12-16 22:01:44 +0530

    Customers Menu Created

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layouts.side');
 });
+
+Route::resource('customers',CustomerController::class);
+Route::resource('services',ServiceController::class);
+Route::resource('Invoice',InvoiceController::class);
